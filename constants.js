db980e232e6fb5154bb38ab27c4c138997dabeef
mariagabrielasalomoncezar-coder <mariagabrielasalomoncezar@gmail.com>
2026-08-14 17:17:28 -0300

    Update WhatsApp link to use hardcoded number

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const WHATSAPP_NUMBER = '5511940317912' // TODO: substituir pelo número real da Viva Conecta
 export const WHATSAPP_LINK = (msg = 'Olá! Quero saber mais sobre os planos Vivo Empresas.') =>
-  `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(msg)}`
+  `https://wa.me/${5511940317912}?text=${encodeURIComponent(msg)}`
 
 export const NAV_LINKS = [
   { label: 'Início', href: '#inicio' },
